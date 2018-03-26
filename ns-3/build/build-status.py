6702070634cfafecb9b74965aa244199778152ec
Michał Król <mharnen@gmail.com>
2018-03-26 15:32:29 +0100

    ignore iles

---
+++ ns-3/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3-dev-timers-debug', 'build/scratch/ns3-dev-scratch-simulator-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-first-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+

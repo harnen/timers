a5146c95222329cfcbf0b1ea73c4401b162ff0cf
Michał Król <mharnen@gmail.com>
2018-04-17 10:12:09 +0100

    renaming simulations

--- ns-3/build/build-status.py
+++ ns-3/build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3-dev-timers-debug', 'build/scratch/ns3-dev-scratch-simulator-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-first-debug']
+ns3_runnable_programs = ['build/scratch/ns3-dev-acks-debug', 'build/scratch/ns3-dev-scratch-simulator-debug', 'build/scratch/subdir/ns3-dev-subdir-debug', 'build/scratch/ns3-dev-thunks-debug', 'build/scratch/ns3-dev-first-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
--- ns-3/scripts/run_tests.sh
+++ ns-3/scripts/run_tests.sh
@@ -8,8 +8,8 @@ for ERR_RATE in 0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1
 do
     echo "Running ERR_RATE=${ERR_RATE}"
     #thunks
-    NS_LOG=ndn.ConsumerThunks ./waf --run="timers -retx=10ms -cDataDelay=5000 -pDataDelay=5000 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/thunks${ERR_RATE}.log
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="timers -retx=10ms -cDataDelay=5000 -pDataDelay=5000 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/thunks${ERR_RATE}.log
     #net time
-    NS_LOG=ndn.ConsumerThunks ./waf --run="timers -retx=1000ms -cDataDelay=1000 -pDataDelay=5000 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/net${ERR_RATE}.log
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="timers -retx=1000ms -cDataDelay=1000 -pDataDelay=5000 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/net${ERR_RATE}.log
 done
 

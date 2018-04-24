15d7cbddc5ab6bb39d31ad1911e828f902eb5f29
Michał Król <mharnen@gmail.com>
2018-04-24 18:49:58 +0100

    scripts

--- ns-3/scripts/generate_graphs.sh
+++ ns-3/scripts/generate_graphs.sh
@@ -4,7 +4,7 @@ declare -A THUNKS_TIME
 declare -A THUNKS_PACKETS
 for file in data/data_thunks*
 do
-    ERR_RATE=$(basename -s ".log" $file | sed -e 's/data_thunks//g' )
+    ERR_RATE=$(basename -s ".log" $file | sed -e 's/data_thunks//g' | cut -d ':' -f 2 )
     COM_TIME=$(grep "Average completion time:" $file | cut -d ':' -f 2)
     PACKETS=$(grep "Average interests sent:" $file | cut -d ':' -f 2)
     echo "$file -> $ERR_RATE -> $COM_TIME"
@@ -16,7 +16,7 @@ declare -A NET_TIME
 declare -A NET_PACKETS
 for file in data/data_net*
 do
-    ERR_RATE=$(basename -s ".log" $file | sed -e 's/data_net//g' )
+    ERR_RATE=$(basename -s ".log" $file | sed -e 's/data_net//g' | cut -d ':' -f 2  )
     COM_TIME=$(grep "Average completion time:" $file | cut -d ':' -f 2)
     PACKETS=$(grep "Average interests sent:" $file | cut -d ':' -f 2)
     echo "$file -> $ERR_RATE -> $COM_TIME"
@@ -32,4 +32,39 @@ for VAL in "${SORTED[@]}"
 do 
     echo  $VAL ${THUNKS_TIME[$VAL]} ${NET_TIME[$VAL]} >> graphs/satisfaction_time.dat
     echo  $VAL ${THUNKS_PACKETS[$VAL]} ${NET_PACKETS[$VAL]} >> graphs/packets.dat
+done
+
+
+declare -A PTHUNKS_TIME
+declare -A PTHUNKS_PACKETS
+for file in data/data_pthunks*
+do
+    APP_TIME=$(basename -s ".log" $file | sed -e 's/data_pthunks//g' | cut -d ':' -f 4 )
+    COM_TIME=$(grep "Average completion time:" $file | cut -d ':' -f 2)
+    PACKETS=$(grep "Average interests sent:" $file | cut -d ':' -f 2)
+    echo "$file -> $APP_TIME -> $COM_TIME"
+    PTHUNKS_TIME[$APP_TIME]=$COM_TIME
+    PTHUNKS_PACKETS[$APP_TIME]=$PACKETS
+done
+
+declare -A PNET_TIME
+declare -A PNET_PACKETS
+for file in data/data_pnet*
+do
+    APP_TIME=$(basename -s ".log" $file | sed -e 's/data_pnet//g' | cut -d ':' -f 4  )
+    COM_TIME=$(grep "Average completion time:" $file | cut -d ':' -f 2)
+    PACKETS=$(grep "Average interests sent:" $file | cut -d ':' -f 2)
+    echo "$file -> $APP_TIME -> $COM_TIME"
+    PNET_TIME[$APP_TIME]=$COM_TIME
+    PNET_PACKETS[$APP_TIME]=$PACKETS
+done
+
+
+IFS=$'\n' SORTED=( $( printf "%s\n" "${!PTHUNKS_TIME[@]}" | sort -n ) )
+echo -e > graphs/psatisfaction_time.dat
+echo -e > graphs/ppackets.dat
+for VAL in "${SORTED[@]}"
+do 
+    echo  $VAL ${PTHUNKS_TIME[$VAL]} ${PNET_TIME[$VAL]} >> graphs/psatisfaction_time.dat
+    echo  $VAL ${PTHUNKS_PACKETS[$VAL]} ${PNET_PACKETS[$VAL]} >> graphs/ppackets.dat
 done
--- ns-3/scripts/run_tests.sh
+++ ns-3/scripts/run_tests.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-TIME=20000
+TIME=10000
 
 cd ..
 
+
+
 for ERR_RATE in 0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1
 do
+    break
     echo "Running ERR_RATE=${ERR_RATE}"
     #thunks, manually remove the RTT
-    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=10ms -cDataDelay=5460 -pDataDelay=5500 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/thunks${ERR_RATE}.log
+    cDelay=5460
+    pDelay=5500
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=10ms -cDataDelay=${cDelay} -pDataDelay=${pDelay} -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/thunks:${ERR_RATE}:${cDelay}:${pDelay}.log
+
     #net time
-    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=1000ms -cDataDelay=1000 -pDataDelay=5500 -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/net${ERR_RATE}.log
+    cDelay=1000
+    pDelay=5500
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=1000ms -cDataDelay=${cDelay} -pDataDelay=${pDelay} -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/net:${ERR_RATE}:${cDelay}:${pDelay}.log
 done
 
+ERR_RATE=0
+for delay in 500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000 8500 9000 9500 10000
+do
+    pDelay=$delay
+    cDelay=$delay
+    let cDelay-=40 
+    echo $cDelay
+    echo "Running Delay=${delay}"
+    
+#thunks, manually remove the RTT
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=10ms -cDataDelay=${cDelay} -pDataDelay=${pDelay} -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/pthunks:${ERR_RATE}:${cDelay}:${pDelay}.log
+
+    #net time
+    cDelay=1000
+    pDelay=$delay
+    NS_LOG=ndn.ProducerThunks:ndn.ConsumerThunks ./waf --run="thunks -retx=1000ms -cDataDelay=${cDelay} -pDataDelay=${pDelay} -time=${TIME}s -errRate=${ERR_RATE}" &> ./scripts/logs/pnet:${ERR_RATE}:${cDelay}:${pDelay}.log
+done

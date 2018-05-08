44d42d3cdbaa27694a6f580eedd4f11cd33bee5e
Michał Król <mharnen@gmail.com>
2018-05-08 16:34:11 +0100

    ACKs

--- ns-3/src/ndnSIM/apps/ndn-consumer-timers-ack.cpp
+++ ns-3/src/ndnSIM/apps/ndn-consumer-timers-ack.cpp
@@ -84,7 +84,7 @@ ConsumerTimersACK::ScheduleNextPacket()
   m_seq++;
   if (m_firstTime) {
 	  NS_LOG_DEBUG("Scheduling next packet in 0 seconds seq:" << m_seq);
-    m_sendEvent = Simulator::Schedule(Seconds(0.0), &ConsumerApp::SendPacket, this, m_interestName, m_seq);
+    m_sendEvent = Simulator::Schedule(Seconds(0.0), &ConsumerACK::SendPacket, this, m_interestName, m_seq);
     m_firstTime = false;
   }
   //we want to keep sending packet no matter what
@@ -92,7 +92,7 @@ ConsumerTimersACK::ScheduleNextPacket()
 	  NS_LOG_DEBUG("Scheduling next packet in " << (1.0 / m_frequency) << " seconds" << "seq: " << m_seq);
     m_sendEvent = Simulator::Schedule((m_random == 0) ? Seconds(1.0 / m_frequency)
                                                       : Seconds(m_random->GetValue()),
-                                      &ConsumerApp::SendPacket, this, m_interestName, m_seq);
+                                      &ConsumerACK::SendPacket, this, m_interestName, m_seq);
   }
 }
 
